99cf6832285edd7f172c0616405d8db1c64cb9e7
noob-analyst <143314299+noob-analyst@users.noreply.github.com>
2023-08-27 08:49:58 +0000

    terraform policy added

--- main.tf
+++ main.tf
@@ -1,11 +1,25 @@
 provider "aws" {
-  region = "ap-southeast-1" # Change to your region
+  region = "ap-southeast-1" # Change to your desired AWS region
 }
 
-resource "aws_s3_bucket" "circleciterraform"{
+resource "aws_s3_bucket" "circleciterraform" {
   bucket = "circleciterraform2" # Change to your unique bucket name
 
   website {
     index_document = "index.html"
   }
+}
+
+resource "aws_s3_bucket_policy" "circleciterraform_policy" {
+  bucket = aws_s3_bucket.circleciterraform.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "s3:GetObject",
+      Effect = "Allow",
+      Resource = aws_s3_bucket.circleciterraform.arn,
+      Principal = "*",
+    }],
+  })
 }
