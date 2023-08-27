3db14c11ee9e97d1870ca6c5e190e1777a4aa75b
noob-analyst <143314299+noob-analyst@users.noreply.github.com>
2023-08-27 09:40:52 +0000

    ass

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-southeast-1" # Change to your desired AWS region
 }
 
-resource "aws_s3_bucket" "circleciterraform" {
+resource "aws_s3_bucket" "circleciterraform2" {
   bucket = "circleciterraform2" # Change to your unique bucket name
 
   website {
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "circleciterraform" {
 }
 
 resource "aws_s3_bucket_policy" "circleciterraform_policy" {
-  bucket = aws_s3_bucket.circleciterraform.id
+  bucket = aws_s3_bucket.circleciterraform2.id
 
   policy = jsonencode({
     Version = "2012-10-17",
