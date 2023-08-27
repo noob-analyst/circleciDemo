2c6dd35214124bc3c31046ba6bbc654c986fcbb0
noob-analyst <143314299+noob-analyst@users.noreply.github.com>
2023-08-27 08:44:21 +0000

    commiting removed acl

--- main.tf
+++ main.tf
@@ -2,9 +2,8 @@ provider "aws" {
   region = "ap-southeast-1" # Change to your region
 }
 
-resource "aws_s3_bucket" "circleciterraform" {
+resource "aws_s3_bucket" "circleciterraform"{
   bucket = "circleciterraform2" # Change to your unique bucket name
-  acl    = "public-read"
 
   website {
     index_document = "index.html"
