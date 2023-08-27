e4456b2b2f3837bb6a73a5777e30ead1c324dc77
noob-analyst <143314299+noob-analyst@users.noreply.github.com>
2023-08-27 13:34:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-southeast-1" # Change to your region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "circleciterraform" {
   bucket = "circleciterraform" # Change to your unique bucket name
   acl    = "public-read"
 
