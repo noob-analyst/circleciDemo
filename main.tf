a5ca29daaa74f8f9ec5399bd394ef9006bdc4e31
noob-analyst <143314299+noob-analyst@users.noreply.github.com>
2023-08-27 11:40:18 +0000

    puncli acl

--- main.tf
+++ main.tf
@@ -11,26 +11,28 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-// Define IAM policy document for public read access
-data "aws_iam_policy_document" "public_read_access" {
-  statement {
-    effect = "Allow"
-    principals {
-      type = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions = ["s3:GetObject"]
-
-    resources = [
-      "${aws_s3_bucket.bucket.arn}/*"
-    ]
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
-
-// Add a policy to the bucket to allow public access
-resource "aws_s3_bucket_policy" "bucket_policy" {
+resource "aws_s3_bucket_public_access_block" "bucket" {
   bucket = aws_s3_bucket.bucket.id
-  policy = data.aws_iam_policy_document.public_read_access.json
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket,
+    aws_s3_bucket_public_access_block.bucket,
+  ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
 }
