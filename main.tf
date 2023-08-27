ba1f6920f89dd7f19a9f9386b1b725681d6cbf41
noob-analyst <143314299+noob-analyst@users.noreply.github.com>
2023-08-27 18:06:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,4 +44,5 @@ resource "aws_s3_object" "index_html" {
   key    = "index.html"
   source = "index.html"  // This assumes index.html is in the root of your
   content_type = "text/html"
+  acl    = "public-read"
 }
