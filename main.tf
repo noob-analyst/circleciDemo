be4ba15702149829ce93796641ae5f6200c29e2e
noob-analyst <143314299+noob-analyst@users.noreply.github.com>
2023-08-27 17:47:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,6 +42,5 @@ resource "aws_s3_bucket_acl" "bucket" {
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.bucket.id
   key    = "index.html"
-  source = "index.html"  // This assumes index.html is in the root of your repository
-  acl    = "public-read"
+  source = "index.html"  // This assumes index.html is in the root of your 
 }
